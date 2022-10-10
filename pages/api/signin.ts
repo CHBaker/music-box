ac495e007473d9ab21e57ed8cdb90d6df2683c91
CHBaker <charlesprobaker@gmail.com>
2022-10-10 14:17:05 -0600

    FEAT: signing api

---
+++ pages/api/signin.ts
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
+import prisma from '../../lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { email, password } = req.body
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign(
+            {
+                email: user.email,
+                id: user.id,
+                time: Date.now(),
+            },
+            'hello',
+            {
+                expiresIn: '8h',
+            }
+        )
+
+        res.setHeader(
+            'Set-Cookie',
+            cookie.serialize('MUSIC_B0X_ACCESS_TOKEN', token, {
+                httpOnly: true,
+                maxAge: 8 * 60 * 60,
+                path: '/',
+                sameSite: 'lax',
+                secure: process.env.NODE_ENV === 'production',
+            })
+        )
+
+        res.json(user)
+    }
+
+    res.status(401)
+    res.json({ error: 'bad creds yo' })
+}
