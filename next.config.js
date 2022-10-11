c2165ddd247daa48bd7c9d6ebf7bbb69195984cb
CHBaker <charlesprobaker@gmail.com>
2022-10-11 11:21:41 -0600

    FEAT: protected routes
    - use next middleware
    - attempt to config source maps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack(config, _) {
+        config.devtool = 'eval-source-map'
+        return config
+    },
 }
 
 module.exports = nextConfig
