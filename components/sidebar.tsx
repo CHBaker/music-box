d643d0888129ee42334f77df453320d6970d79a2
CHBaker <charlesprobaker@gmail.com>
2022-10-13 11:01:26 -0600

    FEAT: playlists.map error handle

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -118,22 +118,23 @@ const Sidebar = () => {
 
                 <Box height='72%' overflowY='auto' paddingY='20px'>
                     <List spacing={2}>
-                        {playlists.map((playlist) => (
-                            <ListItem paddingX='20px' key={playlist.id}>
-                                <LinkBox cursor='pointer'>
-                                    <NextLink
-                                        href={{
-                                            pathname: '/playlist/[id]',
-                                            query: { id: playlist.id },
-                                        }}
-                                        passHref>
-                                        <LinkOverlay>
-                                            {playlist.name}
-                                        </LinkOverlay>
-                                    </NextLink>
-                                </LinkBox>
-                            </ListItem>
-                        ))}
+                        {playlists?.length > 0 &&
+                            playlists?.map((playlist) => (
+                                <ListItem paddingX='20px' key={playlist.id}>
+                                    <LinkBox cursor='pointer'>
+                                        <NextLink
+                                            href={{
+                                                pathname: '/playlist/[id]',
+                                                query: { id: playlist.id },
+                                            }}
+                                            passHref>
+                                            <LinkOverlay>
+                                                {playlist.name}
+                                            </LinkOverlay>
+                                        </NextLink>
+                                    </LinkBox>
+                                </ListItem>
+                            ))}
                     </List>
                 </Box>
             </Box>
